079f21966396a31afc56bb112d70e638ab64c1a4
Aaron M <ajm@powderbluecrayon.com>
2024-08-25 16:56:36 -0500

    inits and configs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module memorygame
+
+go 1.22.5
+
+require github.com/a-h/templ v0.2.771 // indirect
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.templ"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  darkMode: ['class', '[data-theme="dim"]'],
+  daisyui: {
+    themes: ["corporate", "dim"],
+  },
+}
